bf43584ca1d5758fa1ce99a63722b2b74a341998
DarkShimy00 <darkshimy00@gmail.com>
2013-07-18 11:21:26 +0200

    Removed unused lines

--- Launcher/src/fr/minepod/launcher/Start.java
+++ Launcher/src/fr/minepod/launcher/Start.java
@@ -5,11 +5,5 @@ import java.io.IOException;
 public class Start {
 	public static void main(String[] args) {
 		new Downloader().Launch();
-		Runtime runTime = Runtime.getRuntime();
-		/*try {
-			Process process = runTime.exec(new String[] {""});
-		} catch (IOException e) {
-			e.printStackTrace();
-		}*/
 	}
 }
