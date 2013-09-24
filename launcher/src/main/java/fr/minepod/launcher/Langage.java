758d5591486e2917c662ce66dea934f49bc2b2e6
DarkShimy00 <darkshimy00@gmail.com>
2013-09-24 21:10:30 +0100

    Removed unused Strings

--- launcher/src/main/java/fr/minepod/launcher/Langage.java
+++ launcher/src/main/java/fr/minepod/launcher/Langage.java
@@ -2,18 +2,13 @@ package fr.minepod.launcher;
 
 public enum Langage {
 	LAUNCHBUTTON("Jouer"),
-	OPENABOUTGUI("A propos"),
-	ABOUTGUINAME("A propos"),
 	DEBUGINFORMATIONS("Informations de debug"),
 	ERROR("Erreur: "),
 	WHEN("En \u00E9tant en train "),
 	WHENDOWNLOADING("En t\u00E9l\u00E9chargeant "),
-	TOOLS("Outils"),
-	OPTIONS("Options"),
 	COMPILEDON("compil\u00E9 le "),
 	DEVELOPMENTVERSION("version de d\u00E9veloppement"),
-	DOINGMAINTHREADTASKS("d'ex\u00E9cuter les t\u00E2ches principales"),
-	WITHBOOTSTRAP("avec le bootstrap version ");
+	DOINGMAINTHREADTASKS("d'ex\u00E9cuter les t\u00E2ches principales");
 	
 	private String name = "";
 	    
