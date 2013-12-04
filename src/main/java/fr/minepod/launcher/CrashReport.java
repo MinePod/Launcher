85bb855b8d65b2982ef1179bb2dd30a6b4f30938
DarkShimy00 <darkshimy00@gmail.com>
2013-12-04 19:15:33 +0100

    Verify if Logger is null when sending CrashReport
    Updated dependency "Utils" to version 1.1.0

--- src/main/java/fr/minepod/launcher/CrashReport.java
+++ src/main/java/fr/minepod/launcher/CrashReport.java
@@ -8,13 +8,16 @@ public class CrashReport {
 	
 	public static void SendReport(String exception, String when) {
 		try {
-			Config.Logger.severe(exception);
+			if(Config.Logger != null)
+				Config.Logger.severe(exception);
 			javax.swing.JOptionPane.showMessageDialog(null, exception + "\n" +  Langage.WHEN.toString() + when + "\n\n\n" + Langage.DEBUGINFORMATIONS.toString() + ":\n\n" + fr.minepod.Utils.Files.ReadFile(DebugFilePath), Langage.ERROR.toString(), javax.swing.JOptionPane.ERROR_MESSAGE);
 		} catch (HeadlessException e) {
-			Config.Logger.severe(e.toString());
+			if(Config.Logger != null)
+				Config.Logger.severe(e.toString());
 			javax.swing.JOptionPane.showMessageDialog(null, e.toString(),  Langage.ERROR.toString(), javax.swing.JOptionPane.ERROR_MESSAGE);
 		} catch (IOException e) {
-			Config.Logger.severe(e.toString());
+			if(Config.Logger != null)
+				Config.Logger.severe(e.toString());
 			javax.swing.JOptionPane.showMessageDialog(null, e.toString(),  Langage.ERROR.toString(), javax.swing.JOptionPane.ERROR_MESSAGE);
 		}
         System.exit(0);
